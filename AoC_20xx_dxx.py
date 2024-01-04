3ecdb1ac71aa07aa9fc6913da241c34be360589e
gweicox <gweicox@yandex.com>
2024-01-04 22:42:00 +0300

    the main template has been added

---
+++ AoC_20xx_dxx.py
@@ -0,0 +1,32 @@
+#🎄❄🎄🌞🎄❄🎄❄🎄❄🎄❄🎄❄🎄❄🎄🎅
+
+with open("20xx_dxx.txt") as f:
+    data = f.read().strip()
+
+#🎄❄🎄❄🎄🌞🎄❄🎄❄🎄❄🎄❄🎄❄🎄⛄
+
+
+def fun_part_1(data):
+    pass
+
+
+def fun_part_1_kind_of_like_improved_or_not(data):
+    pass
+
+
+#🎄❄🎄❄🎄❄🎄🌞🎄❄🎄❄🎄❄🎄❄🎄🥳
+
+
+def fun_part_2(data):
+    pass
+
+
+def fun_part_2_kind_of_like_improved_or_not(data):
+    pass
+
+
+#🎄❄🎄❄🎄❄🎄❄🎄🌞🎄❄🎄❄🎄❄🎄🧝
+
+print(f"Part 1: {fun_part_1(data)}") # _ ☃️
+print(f"Part 2: {fun_part_2(data)}") # _ 🥂
+# Python 3.12.1
